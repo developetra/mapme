8350c652e918682df9adb4630b51dd1c26c4370f
Thomas Heinz <mail@th1z.net>
2020-04-22 15:52:24 +0200

    Added ThreadPolicy

--- MapMe/app/src/main/java/com/example/mapme/backend/AppService.java
+++ MapMe/app/src/main/java/com/example/mapme/backend/AppService.java
@@ -11,6 +11,7 @@ import android.location.LocationManager;
 import android.os.Binder;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.os.StrictMode;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.util.Log;
@@ -95,6 +96,10 @@ public class AppService extends Service {
         getDataFromDatabase();
 
         // TEST
+        // TODO: Do not run request in main thread:
+        // https://stackoverflow.com/questions/6343166/how-to-fix-android-os-networkonmainthreadexception
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
         getOverpassResult();
 
         Log.d("info", "AppService started");
