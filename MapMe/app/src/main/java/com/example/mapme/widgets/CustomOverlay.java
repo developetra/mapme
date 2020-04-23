b51485774e8ac2a523670cadcd8a117b9fddc453
Thomas Heinz <mail@th1z.net>
2020-04-23 09:11:34 +0200

    Hit test for custom overlay

--- MapMe/app/src/main/java/com/example/mapme/widgets/CustomOverlay.java
+++ MapMe/app/src/main/java/com/example/mapme/widgets/CustomOverlay.java
@@ -2,6 +2,7 @@ package com.example.mapme.widgets;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Rect;
 import android.util.Log;
 import android.view.MotionEvent;
 
@@ -10,7 +11,9 @@ import com.example.mapme.activities.EditInformationActivity;
 import org.osmdroid.bonuspack.kml.KmlDocument;
 import org.osmdroid.bonuspack.kml.KmlFeature;
 import org.osmdroid.bonuspack.kml.Style;
+import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.Projection;
 import org.osmdroid.views.overlay.FolderOverlay;
 import org.osmdroid.views.overlay.Overlay;
 
@@ -26,12 +29,20 @@ public class CustomOverlay extends FolderOverlay {
     @Override
     public boolean onLongPress(final MotionEvent e, final MapView mapView) {
         // TODO:
-        Activity activity = (Activity) mapView.getContext();
-        Intent intent = new Intent(activity, EditInformationActivity.class);
-        intent.putExtra("name", "Edit Object");
-        intent.putExtra("id", id);
-        activity.startActivity(intent);
-        Log.d("Overlay", "onLongPress "+activity);
-        return true;
+        if(hitTest(e, mapView)) {
+            Activity activity = (Activity) mapView.getContext();
+            Intent intent = new Intent(activity, EditInformationActivity.class);
+            intent.putExtra("name", "Edit Object");
+            intent.putExtra("id", id);
+            activity.startActivity(intent);
+            Log.d("Overlay", "onLongPress "+activity);
+            return true;
+        }
+        return false;
+    }
+
+    protected boolean hitTest(final MotionEvent e, final MapView mapView){
+        GeoPoint pt = (GeoPoint) mapView.getProjection().fromPixels((int) e.getX(), (int) e.getY(), null);
+        return this.getBounds().contains(pt);
     }
 }
