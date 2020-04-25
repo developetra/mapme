4733342f64f7a38d3752a8e261c157bd2fa2f0c5
Petra L <petra.langenbacher@stud.uni-bamberg.de>
2020-04-26 00:51:10 +0200

    InfoDialog before editing Object

--- MapMe/app/src/main/java/com/example/mapme/widgets/CustomOverlay.java
+++ MapMe/app/src/main/java/com/example/mapme/widgets/CustomOverlay.java
@@ -1,28 +1,24 @@
 package com.example.mapme.widgets;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Rect;
 import android.util.Log;
 import android.view.MotionEvent;
 
 import com.example.mapme.activities.EditInformationActivity;
 
-import org.osmdroid.bonuspack.kml.KmlDocument;
-import org.osmdroid.bonuspack.kml.KmlFeature;
-import org.osmdroid.bonuspack.kml.Style;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
-import org.osmdroid.views.Projection;
 import org.osmdroid.views.overlay.FolderOverlay;
-import org.osmdroid.views.overlay.Overlay;
 
 public class CustomOverlay extends FolderOverlay {
 
-    private final String id;
+    private final String currentGeoObjectId;
 
     public CustomOverlay(String id){
-        this.id = id;
+        this.currentGeoObjectId = id;
     }
 
 
@@ -30,12 +26,28 @@ public class CustomOverlay extends FolderOverlay {
     public boolean onLongPress(final MotionEvent e, final MapView mapView) {
         // TODO:
         if(hitTest(e, mapView)) {
-            Activity activity = (Activity) mapView.getContext();
-            Intent intent = new Intent(activity, EditInformationActivity.class);
-            intent.putExtra("name", "Edit Object");
-            intent.putExtra("id", id);
-            activity.startActivity(intent);
-            Log.d("Overlay", "onLongPress "+activity);
+
+            AlertDialog.Builder infoDialog = new AlertDialog.Builder(mapView.getContext());
+            infoDialog.setTitle("GeoObject (Id: " + currentGeoObjectId + ")");
+            infoDialog.setMessage("Do you want to edit the GeoObject?");
+            infoDialog.setNegativeButton("Cancel",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    });
+            infoDialog.setNeutralButton("Edit",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            Activity activity = (Activity) mapView.getContext();
+                            Intent intent = new Intent(activity, EditInformationActivity.class);
+                            intent.putExtra("name", "Edit Object");
+                            intent.putExtra("id", currentGeoObjectId);
+                            activity.startActivity(intent);
+                            Log.d("Overlay", "onLongPress "+activity);
+                        }
+                    });
+            infoDialog.show();
             return true;
         }
         return false;
